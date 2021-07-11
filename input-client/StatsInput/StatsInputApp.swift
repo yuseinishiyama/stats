d54a36ea4e7374afed9c30028d1162b190f9fec1
Yusei Nishiyama <yusei.nishiyama@soundcloud.com>
2021-07-11 23:44:11 +0200

    init iOS input client
    Signed-off-by: Yusei Nishiyama <yusei.nishiyama@soundcloud.com>

---
+++ input-client/StatsInput/StatsInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatsInputApp.swift
+//  StatsInput
+//
+//  Created by Yusei Nishiyama on 11.07.21.
+//
+
+import SwiftUI
+
+@main
+struct StatsInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
