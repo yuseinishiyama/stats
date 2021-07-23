558b8d735e1064ad00e8f6f6a289923ff75348e5
Yusei Nishiyama <yusei.nishiyama@soundcloud.com>
2021-07-23 03:57:49 +0200

    terraform s3 backend, closes #6
    Signed-off-by: Yusei Nishiyama <yusei.nishiyama@soundcloud.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "yuseinishiyama-terraform-remote-state"
+    key     = "github.com/yuseinishiyama/stats.tfstate"
+    region  = "us-east-1"
+    profile = "private"
+  }
 }
 
 provider "aws" {
