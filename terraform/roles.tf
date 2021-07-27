95cee0cdba28b90d0aa97dba63c7c2ffc5dc5871
Yusei Nishiyama <yusei.nishiyama@soundcloud.com>
2021-07-27 19:39:57 +0200

    App Runner
    Signed-off-by: Yusei Nishiyama <yusei.nishiyama@soundcloud.com>

--- terraform/roles.tf
+++ terraform/roles.tf
@@ -1,9 +1,19 @@
-data "aws_iam_policy_document" "assume_role" {
+data "aws_iam_policy_document" "ecs_assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
       type        = "Service"
       identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "app_runner_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["build.apprunner.amazonaws.com"]
     }
   }
 }
@@ -22,7 +32,7 @@ data "aws_iam_policy_document" "ssm_messages" {
 
 resource "aws_iam_role" "task" {
   name               = "ecsTaskRole"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role.json
   inline_policy {
     name   = "SSMMessages"
     policy = data.aws_iam_policy_document.ssm_messages.json
@@ -31,6 +41,12 @@ resource "aws_iam_role" "task" {
 
 resource "aws_iam_role" "task_execution" {
   name                = "ecsTaskExecutionRole"
-  assume_role_policy  = data.aws_iam_policy_document.assume_role.json
+  assume_role_policy  = data.aws_iam_policy_document.ecs_assume_role.json
   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"]
+}
+
+resource "aws_iam_role" "ecr_access" {
+  name                = "AppRunnerECRAccessRole"
+  assume_role_policy  = data.aws_iam_policy_document.app_runner_assume_role.json
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"]
 }
--- terraform/task.tf
+++ terraform/task.tf
@@ -34,26 +34,28 @@ resource "aws_cloudwatch_event_rule" "worker" {
 }
 
 # bot
-resource "aws_ecs_task_definition" "bot" {
-  family                   = "bot"
-  task_role_arn            = aws_iam_role.task.arn
-  execution_role_arn       = aws_iam_role.task_execution.arn
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  container_definitions    = file("containers/bot.json")
+resource "aws_apprunner_service" "bot" {
+  service_name = "stats-bot"
+
+  source_configuration {
+    authentication_configuration {
+      access_role_arn = aws_iam_role.ecr_access.arn
+    }
+    image_repository {
+      image_configuration {
+        port          = "80"
+        start_command = "bot"
+        runtime_environment_variables = {
+          FB_VERIFY_TOKEN = var.fb_verify_token
+        }
+      }
+      image_identifier      = "${aws_ecr_repository.this.repository_url}:latest"
+      image_repository_type = "ECR"
+    }
+  }
 }
 
-resource "aws_ecs_service" "bot" {
-  name            = "bot"
-  cluster         = aws_ecs_cluster.this.id
-  task_definition = aws_ecs_task_definition.bot.arn
-  launch_type     = "FARGATE"
-  desired_count   = 1
-  network_configuration {
-    subnets          = [data.aws_subnet.container.id]
-    security_groups  = [data.aws_security_group.container.id]
-    assign_public_ip = true
-  }
+variable "fb_verify_token" {
+  description = "FB Messenger verification token"
+  type        = string
 }
