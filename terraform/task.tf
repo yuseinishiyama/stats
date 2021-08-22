842957531fe95dcc86898607075c3ee0b9e7ae58
Yusei Nishiyama <yusei.nishiyama@soundcloud.com>
2021-08-23 00:24:44 +0200

    disable ECS based worker in favour of https://github.com/yuseinishiyama/home
    Signed-off-by: Yusei Nishiyama <yusei.nishiyama@soundcloud.com>

--- terraform/task.tf
+++ terraform/task.tf
@@ -10,29 +10,6 @@ resource "aws_ecs_task_definition" "worker" {
   container_definitions    = file("containers/worker.json")
 }
 
-resource "aws_cloudwatch_event_target" "worker" {
-  target_id = "StatsWorker"
-  rule      = aws_cloudwatch_event_rule.worker.name
-  arn       = aws_ecs_cluster.this.arn
-  # use auto-generated role. terraform managed role didn't work
-  role_arn = "arn:aws:iam::921647845311:role/ecsEventsRole"
-  ecs_target {
-    launch_type         = "FARGATE"
-    task_count          = 1
-    task_definition_arn = aws_ecs_task_definition.worker.arn
-    network_configuration {
-      subnets          = [data.aws_subnet.container.id]
-      security_groups  = [data.aws_security_group.container.id]
-      assign_public_ip = true
-    }
-  }
-}
-
-resource "aws_cloudwatch_event_rule" "worker" {
-  name                = "stats-worker"
-  schedule_expression = "cron(0 0/12 * * ? *)"
-}
-
 # bot
 resource "aws_apprunner_service" "bot" {
   service_name = "stats-bot"
